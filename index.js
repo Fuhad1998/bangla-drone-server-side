6fa00836bf70734975662af7743b80b6d3f6fe72
fuhad1998 <fuhadrare444@gmail.com>
2022-04-06 13:46:19 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u15fw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+
+    try{
+        await client.connect();
+        const database = client.db('online_shop')
+        const productCollection = database.collection('products')
+    }
+    finally{
+        // await client.close()
+    }
+
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res)=>{
